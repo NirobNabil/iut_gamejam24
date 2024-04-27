dc919985ff82f8aae38633af6fc6981338013668
Arban Hossain <arbanhossain@gmail.com>
2024-04-27 11:57:19 +0600

    final tutorial

--- scripts/landing.gd
+++ scripts/landing.gd
@@ -22,3 +22,12 @@ func _on_play_button_up(button):
 
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://world.tscn")
+
+
+func _on_tuto_pressed():
+	$Tutorial.modulate = Color(1, 1, 1, 1)
+	$Tutorial.mouse_filter = 0
+
+func _on_go_back_pressed():
+	$Tutorial.modulate = Color(0, 0, 0, 0)
+	$Tutorial.mouse_filter = 1
